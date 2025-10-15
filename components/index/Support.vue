896fb6c2ff00da7aaea065edc0c0854ce4951254
Taiyou06 <kaandindar21@gmail.com>
2025-10-15 11:23:40 +0200

    adjust icon sizes

--- components/index/Support.vue
+++ components/index/Support.vue
@@ -17,7 +17,9 @@
           <div class="flex-1 bg-[var(--md-sys-color-surface)] p-8 flex flex-col justify-between">
             <div class="flex-1">
               <div class="flex items-center justify-center h-16 w-16 rounded-full bg-[#FFDD00] text-black mx-auto">
-                <i class="fa-solid fa-mug-hot text-3xl"></i>
+            <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+              <path d="M294.314667 0L242.432 119.424H165.418667v107.648h29.653333L225.152 418.133333H178.005333l62.293334 351.146667 40.021333-0.426667 40.192 255.146667h380.501333l2.645334-17.066667 37.546666-238.08 37.888 0.426667 62.293334-351.189333h-45.056l30.08-191.018667h32.256V119.466667h-81.834667L724.906667 0H294.314667z m22.528 34.346667h385.834666l32.896 75.946666H283.818667l33.024-75.946666z m-117.333334 119.338666H824.32v39.253334H199.509333v-39.253334z m19.328 298.581334h581.76l-50.176 282.453333-241.024-2.56-240.469333 2.56-50.090667-282.453333z"/>
+            </svg>
               </div>
               <h3 class="mt-6 text-xl font-bold text-center text-[var(--md-sys-color-on-surface)]">Buy Me a Coffee</h3>
               <p class="mt-4 text-base text-center text-[var(--md-sys-color-outline)]">A simple and direct way to support our project with a virtual coffee.</p>
@@ -35,7 +37,7 @@
           <div class="flex-1 bg-[var(--md-sys-color-surface)] p-8 flex flex-col justify-between">
             <div class="flex-1">
               <div class="flex items-center justify-center h-16 w-16 rounded-full bg-[#13C3FF] text-white mx-auto">
-                <nuxt-img src="/img/kofi_logo.svg" alt="Ko-fi Logo" class="w-9 h-9" />
+                <nuxt-img src="/img/kofi_logo.svg" alt="Ko-fi Logo" class="w-12 h-12" />
               </div>
               <h3 class="mt-6 text-xl font-bold text-center text-[var(--md-sys-color-on-surface)]">Ko-fi</h3>
               <p class="mt-4 text-base text-center text-[var(--md-sys-color-outline)]">Support us with a one-time donation or become a monthly member.</p>
